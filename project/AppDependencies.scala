d53fb1d84552b719c712f25416690543ab040ac7
John Green <john.green@digital.hmrc.gov.uk>
2024-02-01 12:14:46 +0000

    APIS-6749: Upgrade to Play 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,45 +6,19 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.20.0"
+  def apply(): Seq[ModuleID] = compile ++ test
 
-  val jacksonVersion         = "2.14.2"
-  val jacksonDatabindVersion = "2.14.2"
-
-  val jacksonOverrides = Seq(
-    "com.fasterxml.jackson.core"     % "jackson-core",
-    "com.fasterxml.jackson.core"     % "jackson-annotations",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310"
-  ).map(_ % jacksonVersion)
-
-  val jacksonDatabindOverrides = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % jacksonDatabindVersion
-  )
-
-  val akkaSerializationJacksonOverrides = Seq(
-    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-cbor",
-    "com.fasterxml.jackson.module"     % "jackson-module-parameter-names",
-    "com.fasterxml.jackson.module"     %% "jackson-module-scala"
-  ).map(_ % jacksonVersion)
-
+  private val bootstrapVersion = "8.4.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "com.auth0"               %  "java-jwt"                   % "4.4.0",
     "org.typelevel"           %% "cats-core"                  % "2.10.0"
-    excludeAll(
-      ExclusionRule("com.fasterxml.jackson.core", "jackson-databind")
-    )
-  ) ++ jacksonDatabindOverrides ++ jacksonOverrides ++ akkaSerializationJacksonOverrides
+  )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion,
-    "org.scalatest"           %% "scalatest"                  % "3.2.17",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
     "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.29",
-    "com.typesafe.play"       %% "play-test"                  % current,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.0",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
     "org.xmlunit"             %  "xmlunit-core"               % "2.9.0"
   ).map(_ % "test, it")
 }
