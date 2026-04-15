1a15f45f57113c36b596b7ebbf2a855c7f59ec98
Neil Frow <675806+worthydolt@users.noreply.github.com>
2026-04-15 09:53:21 +0100

    API-9051: add controller for EORI messages and hard-code success response (#77)
    * API-8985: add sequence diagram for with and without attachment processing
    
    * API-9051: add controller for EORI messages and hard-code success response
    
    * API-9051: format and tidy
    
    * API-9051 - Changing test name from certex to eori
    
    * API-9051: whitespace only
    
    ---------
    
    Co-authored-by: peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>

---
+++ app/uk/gov/hmrc/apiplatforminboundsoap/controllers/eori/EoriMessageController.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apiplatforminboundsoap.controllers.eori
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future.successful
+import scala.xml.NodeSeq
+
+import cats.data.NonEmptyList
+
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import uk.gov.hmrc.apiplatforminboundsoap.controllers.actionBuilders.{SoapErrorResponse, VerifyJwtTokenAction}
+import uk.gov.hmrc.apiplatforminboundsoap.models.{SendFailExternal, SendNotAttempted, SendSuccess}
+import uk.gov.hmrc.apiplatforminboundsoap.services.InboundEoriMessageService
+
+@Singleton()
+class EoriMessageController @Inject() (
+    cc: ControllerComponents,
+    verifyJwtTokenAction: VerifyJwtTokenAction,
+    inboundEoriMessageService: InboundEoriMessageService
+  )(implicit ec: ExecutionContext
+  ) extends BackendController(cc) with SoapErrorResponse {
+
+  def message(): Action[NodeSeq] = (Action andThen verifyJwtTokenAction).async(parse.xml) {
+    implicit request =>
+      val requestId = request.headers.get("x_request_id").getOrElse("unable to obtain x_request_id")
+      inboundEoriMessageService.processInboundMessage(request.body) flatMap {
+        case SendSuccess(status, body)         => successful(Status(status)(body).as("application/soap+xml"))
+        case SendFailExternal(message, status) =>
+          successful(returnErrorResponse(NonEmptyList.one(message), requestId, status))
+        case SendNotAttempted(message)         =>
+          successful(returnErrorResponse(NonEmptyList.one(message), requestId))
+      }
+  }
+}
